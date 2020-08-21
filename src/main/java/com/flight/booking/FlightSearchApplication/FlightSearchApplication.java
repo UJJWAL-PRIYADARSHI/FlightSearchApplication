7905023175001eac2fc8f6875a0549f373e48c09
ujjwal <ujjwal@192.168.1.6>
2020-08-21 17:49:07 +0530

    initail commit

---
+++ src/main/java/com/flight/booking/FlightSearchApplication/FlightSearchApplication.java
@@ -0,0 +1,13 @@
+package com.flight.booking.FlightSearchApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flight/booking/FlightSearchApplication/FlightSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flight.booking.FlightSearchApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
